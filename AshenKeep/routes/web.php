c48b6f8efb736ae6998123fee8315b80f1af0756
jmmiyabe <jmmiyabe@student.apc.edu.ph>
2024-12-28 21:36:44 +0800

    forced https in links

--- AshenKeep/routes/web.php
+++ AshenKeep/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+
+$url = config('app.url');
+URL::forceRootUrl($url);
 
 Route::get('/', function () {
     return view('welcome');
